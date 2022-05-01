e12725b05184078539eb3e417bd559d6fdbd954d
zhanxuejun <905159@zxj>
2022-05-01 19:30:16 +0800

    jdk18的demo

--- JDK/jdk-feature/src/main/java/com/example/jdk18/HttpServerDemo.java
+++ JDK/jdk-feature/src/main/java/com/example/jdk18/HttpServerDemo.java
@@ -1,14 +1,20 @@
 package com.example.jdk18;
 
 
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.SimpleFileServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.file.Path;
+
 public class HttpServerDemo {
 
-    public static void main(String[] args) {
-//        var filter = SimpleFileServer.createOutputFilter(System.out,
-//          OutputLevel.INFO);
-//
-//        var server = HttpServer.create(new InetSocketAddress(8080),
-//       10, "/store/", new SomePutHandler());
-
+    public static void main(String[] args) throws IOException {
+        var handler = SimpleFileServer.createFileHandler(Path.of("/some/path"));
+        var server = HttpServer.create(new InetSocketAddress(8080),
+     10, "/store/", handler);
+        server.createContext("/browse/", handler);
+        server.start();
     }
 }
