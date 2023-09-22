b01a2d821c3e594c8aead7df854f5b06c5e95ae1
xjz18 <zhanxj@bxlsj.com>
2023-09-22 16:54:29 +0800

    feat: 新增jdk21的特性

--- JDK/jdk-feature/src/main/java/com/example/jdk20/PatternTest.java
+++ JDK/jdk-feature/src/main/java/com/example/jdk20/PatternTest.java
@@ -16,13 +16,12 @@ public class PatternTest {
     }
 
     static String formatterPatternSwitch(Object obj) {
-        return null;
-//        return switch (obj) {
-//            case Integer i -> String.format("int %d", i);
-//            case Long l    -> String.format("long %d", l);
-//            case Double d  -> String.format("double %f", d);
-//            case String s  -> String.format("String %s", s);
-//            default        -> obj.toString();
-//        };
+        return switch (obj) {
+            case Integer i -> String.format("int %d", i);
+            case Long l    -> String.format("long %d", l);
+            case Double d  -> String.format("double %f", d);
+            case String s  -> String.format("String %s", s);
+            default        -> obj.toString();
+        };
     }
 }
